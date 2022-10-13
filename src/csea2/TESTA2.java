220233c4571b046070e8e051d5483b96f068622a
ACOECSE-DEVOPS <113933870+ACOECSE-DEVOPS@users.noreply.github.com>
2022-10-13 15:35:17 +0530

    hiiiiiiiiiii

--- src/csea2/TESTA2.java
+++ src/csea2/TESTA2.java
@@ -5,7 +5,8 @@ public class TESTA2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("HI WELCOME TO MY LAB from DURGA PRASAD from rajahmundry");
-		System.out.print("DURGA PRASAD HIIIIIIIII");
+		System.out.print("DURGA PRASAD HIIIIIIIII BYEEEEEEEEEEEEEEE");
+		
 
 	}
 
