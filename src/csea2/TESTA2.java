de8351e396c53373396d0b67d7a45640cf5b66d4
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-10-13 15:33:59 +0530

    dp added

--- src/csea2/TESTA2.java
+++ src/csea2/TESTA2.java
@@ -5,6 +5,7 @@ public class TESTA2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("HI WELCOME TO MY LAB from DURGA PRASAD from rajahmundry");
+		System.out.print("DURGA PRASAD HIIIIIIIII");
 
 	}
 
